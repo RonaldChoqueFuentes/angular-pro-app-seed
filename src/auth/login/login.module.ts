1a29016e42aa03377f037af1857336c8d12e8311
Ronald Choque Fuentes <Ronald Choque@jalasoft.local>
2018-03-14 13:59:11 -0400

    lasy loading  structure

---
+++ src/auth/login/login.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './Containers/login/login.component';
+
+export const ROUTES:Routes = [
+    {
+        path:'',component:LoginComponent
+    }
+   ];
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+         RouterModule
+        ],
+    declarations: [
+        LoginComponent
+    ],
+
+})
+export class LoginModule {}
