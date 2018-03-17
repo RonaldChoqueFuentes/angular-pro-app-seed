ef106ea303ff3d304efd4289616d6c788843ab90
Ronald Choque Fuentes <Ronald Choque@jalasoft.local>
2018-03-17 12:00:17 -0400

    meals form  create

--- src/health/meals/meals.module.ts
+++ src/health/meals/meals.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
+import { MealFormComponent } from './components/meal-form/meal-form.component';
 
 import {MealsComponent} from './containers/meals/meals.component';
 import {MealComponent} from './containers/meal/meal.component';
@@ -24,7 +25,8 @@ export const ROUTES:Routes = [
     ],
     declarations: [
         MealsComponent,
-        MealComponent
+        MealComponent,
+        MealFormComponent
     ]
 })
 export class MealsModule {}
