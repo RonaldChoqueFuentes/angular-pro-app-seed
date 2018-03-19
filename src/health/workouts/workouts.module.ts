c4e7dea7cb3221a9770785b98481c33126d518b3
Ronald Choque Fuentes <Ronald Choque@jalasoft.local>
2018-03-19 10:52:09 -0400

    add workouts changes

--- src/health/workouts/workouts.module.ts
+++ src/health/workouts/workouts.module.ts
@@ -4,22 +4,30 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
+import { WorkoutFormComponent } from './components/workout-form/workout-form.component';
 
 import {WorkoutsComponent} from './containers/workouts/workouts.component';
+import {WorkoutComponent} from './containers/workout/workout.component';
 
+import { SharedModule } from '../shared/shared.module';
 
 export const ROUTES:Routes = [
- { path:'', component:WorkoutsComponent }
+ { path:'', component:WorkoutsComponent },
+ { path:'new', component:WorkoutComponent },
+ { path:':id', component:WorkoutComponent },
 ];
 
 @NgModule({
     imports: [
         CommonModule,
         ReactiveFormsModule,
-        RouterModule.forChild(ROUTES)
+        RouterModule.forChild(ROUTES),
+        SharedModule
     ],
     declarations: [
-        WorkoutsComponent
+        WorkoutsComponent,
+        WorkoutComponent,
+        WorkoutFormComponent
     ]
 })
 export class WorkoutsModule {}
