95e19b4deb322f0c978f34728ec566e713c0468a
Ronald Choque Fuentes <Ronald Choque@jalasoft.local>
2018-03-21 14:02:24 -0400

    schedule days

--- src/health/schedule/components/schedule-days/schedule-days.component.ts
+++ src/health/schedule/components/schedule-days/schedule-days.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output,EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
     selector: 'schedule-days',
+    changeDetection:ChangeDetectionStrategy.OnPush,
     styleUrls: ['schedule-days.component.scss'],
     template: `
-        <div>
-            
+        <div class= "days">
+          <button 
+            type = "button"
+            class="day" 
+            *ngFor="let day of days; index as i;"
+            (click)="selectedDay(i)"
+            >
+            <span [class.active]= "i===selected" >
+            {{ day }}
+            </span>
+          </button>
         </div>
     `
 })
 export class ScheduleDaysComponent {
+
+    days =  ['M','T','W','T','F','S','S'];
+
+    @Input()
+    selected:number;
+
+    @Output()
+    select = new EventEmitter<number>();
     constructor() {}
+
+    selectedDay(index:number){
+        this.select.emit(index);
+    }
 }
