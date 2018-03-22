0debdd13080d265e91788b98dd13deed4cf02aec
Ronald Choque Fuentes <Ronald Choque@jalasoft.local>
2018-03-22 16:12:30 -0400

    sections

--- src/health/schedule/schedule.module.ts
+++ src/health/schedule/schedule.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
+import { SharedModule } from '../shared/shared.module';
+
 //Components
 import { ScheduleCalendarComponent } from './components/schedule-calendar/schedule-calendar.component';
 import { ScheduleControlsComponent } from './components/schedule-controls/schedule-controls.component';
 import { ScheduleDaysComponent } from './components/schedule-days/schedule-days.component';
+import { ScheduleSectionComponent } from './components/schedule-section/schedule-section.component';
 
 // containers
 import { ScheduleComponent } from './containers/schedule/schedule.component';
+
 
 
 export const ROUTES: Routes = [
@@ -21,13 +25,15 @@ export const ROUTES: Routes = [
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    RouterModule.forChild(ROUTES)
+    RouterModule.forChild(ROUTES),
+    SharedModule
   ],
   declarations: [
     ScheduleComponent,
     ScheduleCalendarComponent,
     ScheduleControlsComponent,
-    ScheduleDaysComponent
+    ScheduleDaysComponent,
+    ScheduleSectionComponent
   ]
 })
 export class ScheduleModule {}
